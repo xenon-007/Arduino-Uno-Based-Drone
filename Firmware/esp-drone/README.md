3160e093081c63f500e1056d2465d656ecc6ad84
Priyanshu Kumar Behera <118827995+xenon-007@users.noreply.github.com>
2025-02-23 23:43:15 +0530

    Firmware for ESP32 based drone

---
+++ Firmware/esp-drone/README.md
@@ -0,0 +1,14 @@
+
+### Introduction
+
+**ESP-Drone** is an open source solution based on Espressif ESP32/ESP32-S2/ESP32-S3 Wi-Fi chip, which can be controlled by a mobile APP or gamepad over **Wi-Fi** connection. ESP-Drone comes with **simple hardware**, **clear and extensible code architecture**, and therefore this project can be used in **STEAM education** and other fields. The main code is ported from **Crazyflie** open source project with **GPL3.0** protocol.
+
+### Features
+
+1. Stabilize Mode
+2. Height-hold Mode
+3. Position-hold Mode
+4. APP Control
+5. CFclient Supported
+
+
---
+++ Firmware/esp-drone/main/main.c
@@ -0,0 +1,34 @@
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/portmacro.h"
+#include "nvs_flash.h"
+
+#include "stm32_legacy.h"
+#include "platform.h"
+#include "system.h"
+#define DEBUG_MODULE "APP_MAIN"
+#include "debug_cf.h"
+
+void app_main()
+{
+    esp_err_t ret = nvs_flash_init();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    ESP_ERROR_CHECK(ret);
+    if (platformInit() == false) {
+        while (1);
+    }
+
+    systemLaunch();
+
+}
